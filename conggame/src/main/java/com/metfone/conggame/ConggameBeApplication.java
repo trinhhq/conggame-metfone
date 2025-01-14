31fe4f9b7600b3d2b74919a8653fe79f2b0e1870
trinhhq <trinhhq1999@gmail.com>
2025-01-14 14:09:34 +0700

    init commit

---
+++ conggame/src/main/java/com/metfone/conggame/ConggameBeApplication.java
@@ -0,0 +1,13 @@
+package com.metfone.conggame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConggameBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConggameBeApplication.class, args);
+	}
+
+}
---
+++ conggame/src/test/java/com/metfone/conggame/ConggameBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metfone.conggame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConggameBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
